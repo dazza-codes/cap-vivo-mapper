8d5e6122f908e81e732aed37c232f7de24cc871a
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-09-11 11:05:36 -0700

    Revisions for VIVO vcard, esp. contact details

--- lib/cap/vivo/mapper.rb
+++ lib/cap/vivo/mapper.rb
@@ -5,7 +5,7 @@ module Cap
 
       HAS_CONTACT_INFO = RDF::URI.parse 'http://purl.obolibrary.org/obo/ARG_2000028'
 
-      PROFILE_URI_PREFIX = 'https://profiles.stanford.edu/vivo'
+      PROFILE_URI_PREFIX = RDF::URI.parse 'https://profiles.stanford.edu/vivo'
 
       # PROV data
       MAPPING_ACTIVITY = RDF::URI.parse('https://github.com/sul-dlss/cap-vivo-mapper/mapping')
@@ -29,7 +29,7 @@ module Cap
         @config = Cap::Vivo.configuration
         @profile = profile
         @id = profile['profileId']
-        @uri = RDF::URI.parse "#{PROFILE_URI_PREFIX}/#{@id}"
+        @uri = PROFILE_URI_PREFIX + "/#{@id}"
         @rdf = RDF::Graph.new
         @rdf << [@uri, RDF.type, RDF::FOAF.Person]
       end
@@ -37,82 +37,161 @@ module Cap
       # Convert CAP profile into VIVO linked data
       def create_vivo
         create_vcard
+        prov
       end
 
       # Map names, addresses, contacts and web links to vcard data.
       def create_vcard
-        vcard = RDF::Node.new
+        vcard = @uri + '/vcard'
         @rdf << [@uri, HAS_CONTACT_INFO, vcard]
         @rdf << [vcard, RDF.type, RDF::Vocab::VCARD.Individual]
         # Names
         profile['names'].each_pair do |type, name|
-          given_name  = name['firstName']
-          family_name = name['lastName']
-          vcard_name = RDF::Node.new
+          fn = name['firstName'] || ''
+          mn = name['middleName'] || ''
+          ln = name['lastName'] || ''
+          vcard_name = vcard + "/names/#{type}"
           @rdf << [vcard, RDF::Vocab::VCARD.hasName, vcard_name]
           @rdf << [vcard_name, RDF.type, RDF::Vocab::VCARD.Name]
           @rdf << [vcard_name, RDF::RDFS.label, type]
-          @rdf << [vcard_name, RDF::Vocab::VCARD.send('given-name'), given_name]
-          @rdf << [vcard_name, RDF::Vocab::VCARD.send('family-name'), family_name]
+          @rdf << [vcard_name, RDF::Vocab::VCARD.send('given-name'), fn]
+          @rdf << [vcard_name, RDF::Vocab::VCARD.send('family-name'), ln]
+          unless mn.empty?
+            @rdf << [vcard_name, RDF::Vocab::VCARD.send('additional-name'), mn]
+          end
         end
         # Addresses
         offices = profile['academicOffices'] || []
         offices.each do |office|
-          vcard_address = RDF::Node.new
+          type = offices.length > 1 ? SecureRandom.hex(5) : office['type']
+          label = office['label'] || office['officeName']
+          vcard_address = vcard + "/academicOffices/#{type}"
           @rdf << [vcard, RDF::Vocab::VCARD.hasAddress, vcard_address]
           @rdf << [vcard_address, RDF.type, RDF::Vocab::VCARD.Address]
           @rdf << [vcard_address, RDF.type, RDF::Vocab::VCARD.Work]
-          @rdf << [vcard_address, RDF::RDFS.label, office['type']]
+          @rdf << [vcard_address, RDF::RDFS.label, label] if label
           country = office['country'] || 'United States'
           @rdf << [vcard_address, RDF::Vocab::VCARD.send('country-name'), country]
           @rdf << [vcard_address, RDF::Vocab::VCARD.region, office['state']]
           @rdf << [vcard_address, RDF::Vocab::VCARD.locality, office['city']]
           @rdf << [vcard_address, RDF::Vocab::VCARD.send('postal-code'), office['zip']]
           @rdf << [vcard_address, RDF::Vocab::VCARD.send('street-address'), office['address']]
-        end
-        # Contacts (email and telephone)
-        contacts = []
-        contacts.push profile['primaryContact']
-        contacts.push profile['alternateContact']
-        contacts.compact!
-        contacts.each do |contact|
-          unless contact['email'].nil?
-            vcard_email = RDF::Node.new
-            @rdf << [vcard, RDF::Vocab::VCARD.hasEmail, vcard_email]
-            @rdf << [vcard_email, RDF.type, RDF::Vocab::VCARD.Work]
-            label = contact['type'] || contact['label'] || ''
-            unless label.empty?
-              @rdf << [vcard_email, RDF::RDFS.label, RDF::Literal.new(label)]
-            end
-            emails = contact['email'].split
-            emails.each do |email|
-              email = email.gsub(/,\z/,'').gsub(/>\z/,'')
-              email = RDF::URI.parse("mailto:#{email}")
-              @rdf << [vcard_email, RDF::Vocab::VCARD.hasValue, email]
+          if office['phoneNumbers']
+            phones = office['phoneNumbers'].map {|p| p.gsub(/\W+/,'') }.to_set
+            phones.each do |p|
+              tel = "tel:#{p}"
+              vcard_phone = vcard_address + "/phoneNumber/#{tel}"
+              @rdf << [vcard_address, RDF::Vocab::VCARD.hasTelephone, vcard_phone]
+              @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Voice]
+              @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Work]
+              @rdf << [vcard_phone, RDF::Vocab::VCARD.hasValue, tel]
             end
           end
-          phone_numbers = contact['phoneNumbers'] || []
-          phone_numbers.each do |phone|
-            number = phone.gsub(/\s+|[()+-]/,'')
-            tel = "tel:#{number}"
-            vcard_phone = RDF::Node.new
-            @rdf << [vcard, RDF::Vocab::VCARD.hasTelephone, vcard_phone]
-            @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Tel]
-            @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Voice]
-            @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Work]
-            @rdf << [vcard_phone, RDF::Vocab::VCARD.hasValue, tel]
+        end
+        # Contacts (email and telephone)
+        contact_types = ['primaryContact', 'alternateContact']
+        contact_types.each do |contact_type|
+          contact = profile[contact_type]
+          if contact
+            contact_uri = vcard + "/#{contact_type}"
+            @rdf << [vcard, RDF::Vocab::VCARD.hasRelated, contact_uri]
+            @rdf << [contact_uri, RDF::RDFS.label, contact_type]
+            @rdf << [contact_uri, RDF.type, RDF::Vocab::VCARD.Contact]
+            @rdf << [contact_uri, RDF.type, RDF::Vocab::VCARD.Work]
+            profile_contact_email(contact, contact_uri)
+            profile_contact_telephone(contact, contact_uri)
           end
         end
         # Web Links
         profiles_url = profile_link('https://cap.stanford.edu/rel/public')
         if profiles_url
-          vcard_url = RDF::Node.new
+          vcard_url = vcard + '/links/public'
           @rdf << [vcard, RDF::Vocab::VCARD.hasURL, vcard_url]
-          @rdf << [vcard_url, RDF::Vocab::VCARD.hasValue, profiles_url]
-          @rdf << [vcard_url, RDF::RDFS.label, "CAP public profile"]
           @rdf << [vcard_url, RDF.type, RDF::Vocab::VCARD.Work]
+          @rdf << [vcard_url, RDF::RDFS.label, "CAP public profile"]
+          @rdf << [vcard_url, RDF::Vocab::VCARD.hasValue, profiles_url]
         end
       end
+
+      # Does this profile have a 'physician' affiliation?
+      def physician?
+        profile['affiliations']['physician']
+      end
+
+      # Extract and add physician data to the VIVO RDF
+      def vcard_physician(vcard)
+        if physician?
+          # Create Vcard data for physician data, e.g.
+
+
+          # "californiaPhysicianLicense"=>"G17XXX",
+          # "clinicalContacts"=>
+          #  [{"address"=>"99999 Watson Ct",
+          #    "address2"=>"MC 9999",
+          #    "city"=>"Palo Alto",
+          #    "fax"=>"(650) 999-9999",
+          #    "name"=>"Jack Rabbit, MD",
+          #    "officeName"=>"Stanford Eyes",
+          #    "phoneNumbers"=>["(650) 999-9999"],
+          #    "state"=>"CA",
+          #    "title"=>"Clinical Associate Professor",
+          #    "zip"=>"94303"}],
+          # "clinicalFocus"=>["Ophthalmology"],
+          # "clinicalPractices"=>
+          #  [{"id"=>"shc",
+          #    "label"=>
+          #     { "text"=>"Stanford Hospital and Clinics"},
+          #    "name"=>"Stanford Hospital and Clinics",
+          #    "url"=>"http://www.stanfordhospital.org"}],
+          # "department"=>"Ophthalmology",
+
+        end
+      end
+
+
+
+      # Extract and add contact email data to the VIVO RDF
+      # @param contact [Hash] profile contact data
+      # @param contact_uri [RDF::URI] profile contact URI
+      def profile_contact_email(contact, contact_uri)
+        if contact['email']
+          vcard_email = contact_uri + '/email'
+          @rdf << [contact_uri, RDF::Vocab::VCARD.hasEmail, vcard_email]
+          @rdf << [vcard_email, RDF.type, RDF::Vocab::VCARD.Work]
+          label = contact['type'] || contact['label'] || ''
+          unless label.empty?
+            @rdf << [vcard_email, RDF::RDFS.label, RDF::Literal.new(label)]
+          end
+          # The 'email' field might contain multiple entries, separated by
+          # various kinds of delimiter and each item is not necessarily
+          # validated, so this is an attempt to clean up some things, but
+          # it's not fool proof.  For more on this topic, see
+          # http://davidcel.is/posts/stop-validating-email-addresses-with-regex/
+          emails = contact['email'].split
+          emails.each do |email|
+            email = email.gsub(/,\z/,'').gsub(/>.*/,'').gsub(/\A</,'')
+            email = RDF::URI.parse("mailto:#{email}")
+            @rdf << [vcard_email, RDF::Vocab::VCARD.hasValue, email]
+          end
+        end
+      end
+
+      # Extract and add contact telephone data to the VIVO RDF
+      # @param contact [Hash] profile contact data
+      # @param contact_uri [RDF::URI] profile contact URI
+      def profile_contact_telephone(contact, contact_uri)
+        if contact['phoneNumbers']
+          phones = contact['phoneNumbers'].map {|p| p.gsub(/\W+/,'') }.to_set
+          phones.each do |p|
+            vcard_phone = contact_uri + "/phone/#{p}"
+            @rdf << [contact_uri, RDF::Vocab::VCARD.hasTelephone, vcard_phone]
+            @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Voice]
+            @rdf << [vcard_phone, RDF.type, RDF::Vocab::VCARD.Work]
+            @rdf << [vcard_phone, RDF::Vocab::VCARD.hasValue, p]
+          end
+        end
+      end
+
 
       # Retrieve profile link URLs, using the link relation, such as:
       # Profiles - 'https://cap.stanford.edu/rel/public'
@@ -169,7 +248,6 @@ module Cap
       end
 
       def save
-        # prov
         @rdf.each_statement {|s| @config.rdf_repo.insert_statement s};
       end
 
