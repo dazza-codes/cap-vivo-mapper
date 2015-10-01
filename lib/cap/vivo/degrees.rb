734c0f8f6410184c667b2a77cdfb8cb478afc197
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-09-30 23:13:03 -0700

    Work in progress to disambiguate degrees

--- lib/cap/vivo/degrees.rb
+++ lib/cap/vivo/degrees.rb
@@ -55,35 +55,47 @@ module Cap
         g
       end
 
-      # Guess the acronym for a degree string
+      # Suggest academic degree acronyms for a degree description that may
+      # contain many variations on degree identifiers; that is, attempt to
+      # disambiguate a degree description, erring on the side of false
+      # positive suggestions.  Among all the suggestions, there may be one
+      # or two that are accurate.
       # @parameter degree [String] a degree designation
-      # @return acronym [String]
-      def degree_acronym(degree)
-        # TODO: exploring parsing the degree for an abbreviation
-        # degrees.map {|d| a = /.\. .*\./.match(d); b = a ? a.to_s.gsub(/[ ]/,'') : d.split.first; [b, d] }
-        # get the first word from the degree, which is often the
-        # degree abbreviation (in some form)
-        m = /.[. ]+.*\./.match(degree)
-        abbr = m ? m.to_s.gsub(/[ ]/,'') : degree.split.first
-        abbr = abbr.chomp('.') + '.'
-        # This might be an acronym already, is it a VIVO degree?
-        return abbr if vivo_degree_abbreviations.key? abbr
-        if abbr.length <= 6
-          # remove all punctuation and capitalize
-          acro = abbr.gsub(/\W+/,'').upcase
-          if vivo_degree_abbreviations.value? acro
-            matches = vivo_degree_abbreviations.select {|k,v| v == acro }.keys
-            return matches.first if matches.length == 1
-            # TODO: figure out what to do with this?
-            # The abbreviation might match multiple labels, so better check the
-            # vivo_degree_abbreviation_ambiguous
+      # @return acronyms [Array<String>]
+      def degree_acronyms(degree)
+
+        # TODO: split the degree on any punctuation, except '.'
+
+        # Find candidate degree labels that contain the degree description.
+        # The degree labels are more authoritative than the degree abbreviations,
+        # because some abbreviations match multiple labels (see the results in
+        # vivo_degree_abbreviation_ambiguous).
+        abbrevs = []
+        if degree.length > 6
+          # the degree might be spelled out, not just an acronym
+          degree_words = wordset(degree.split)
+          vivo_degree_wordsets.each_pair do |abbrev, words|
+            # only match when all the vivo degree words are in degree
+            i = words.intersection(degree_words)
+            if i.length == words.length
+              # Good, all the vivo words are in this degree description;
+              # but the degree description could contain more words.
+              if words.length == degree_words.length
+                # This is an exact match, stop here.
+                return [abbrev]
+              else
+                # This is a good candidate, although not an exact match.
+                abbrevs.push(abbrev)
+              end
+            end
           end
-          # nasty little hack, might work sometimes.
-          # return acro.chars.join('.') + '.'
-        else
-          # this might be a full title
         end
-        nil  # shoot!
+        # Find candidate degree acronyms in the degree description;
+        # e.g. a='MA' and {k: v} = {'M.A.' => 'MA'}, return 'M.A.'
+        # Match at the beginning or the end of the degree description.
+        a = degree.gsub(/\W*/,'').upcase
+        abbrevs.push vivo_degree_abbreviations.select {|k,v| a =~ /^#{v}|#{v}$/}.keys
+        abbrevs.flatten.compact.uniq
       end
 
       # Create a URI for a degree string
@@ -113,11 +125,9 @@ module Cap
       # @return abbreviations [Hash] VIVO degree abbreviations
       def vivo_degree_abbreviations
         @vivo_degree_abbreviations ||= begin
-          h = {}
           q = [nil, RDF::VIVO.abbreviation, nil]
-          a = vivo_degrees.query(q).objects.to_a
-          a.each {|i| s = i.to_s; h[s] = vivo_degree_acronym(s) }
-          h
+          abbrevs = vivo_degrees.query(q).objects
+          abbrevs.map {|o| [o.to_s, vivo_degree_acronym(o.to_s)] }.to_h
         end
       end
 
@@ -133,7 +143,7 @@ module Cap
       def vivo_degree_abbreviation_ambiguous
         @vivo_degree_abbreviation_ambiguous ||= begin
           h = {}
-          vivo_degree_abbreviations.keys.map do |a|
+          vivo_degree_abbreviations.keys.each do |a|
             # select all labels that use this abbreviation
             labels = vivo_degree_labels.select {|l| l.split.first == a }
             h[a] = labels if labels.length > 1
@@ -147,9 +157,53 @@ module Cap
       def vivo_degree_labels
         @vivo_labels ||= begin
           q = [nil, RDF::RDFS.label, nil]
-          a = vivo_degrees.query(q).objects.to_a
-          a.map {|i| i.to_s }.sort
+          labels = vivo_degrees.query(q).objects
+          labels.map {|i| i.to_s }.sort
         end
+      end
+
+      # VIVO degree word sets are a hash with the degree abbreviation keys
+      # and the values are significant words that identify the degree (lowercase)
+      # The data structure can be used in analysis to disambiguate a degree label.
+      # @return degree_wordsets [Hash<String:Set<String>>]
+      def vivo_degree_wordsets
+        @vivo_degree_wordsets ||= begin
+          labels = vivo_degree_labels
+          labels.map {|l| arr = l.split; [arr.first, wordset(arr[1..-1])]}.to_h
+        end
+      end
+
+      # A hash of academic degrees {abbreviation: description};
+      # the data is from an MIT web site, but not necessarily MIT degrees.
+      # @return mit_degrees [Hash<String:String>]
+      def mit_degrees
+        @mit_degrees ||= begin
+          degrees = "AA - Associate in Arts\nAB - Bachelor in Arts\nAE - Aeronautical Engineer\nAM - Master of Arts\nAS - Associate in Science\nAN - Associate in Nursing\nBAC - Bachelor of Accountancy\nBAR - Bachelor in Architecture\nBBA - Bachelor in Business Administration\nBCH - Bachelor of Chemistry\nBCP - Bachelor in City Planning\nBE - Building Engineer\nBEC - Bachelor of Economics\nBED - Bachelor of Education\nBFA - Bachelor of Fine Arts\nBO - Non Degree Bachelors\nBOC - Bachelor of Commerce\nBOE - Bachelor of Engineering\nBS - Bachelor of Science\nBSE - Bachelor of Engineering\nBTE - Bachelor of Technology\nBTS - Bachelor of Theological Studies\nCE - Civil Engineer\nCHE - Chemical Engineer\nCPH - Certificate of Public Health\nCSE - Engineering in Computer Science\nCTF - Certificate\nDBA - Doctor of Business Adminstration\nDDS - Doctor of Dental Science\nDIE - Diploma of Engineering\nDIP - Diploma\nDMD - Doctor of Medical Dentistry\nDO - Non Degree Doctorals\nDPH - Doctor of Public Health\nDSC - Doctor of Science\nDVM - Doctor of Veterinary Medicine\nEAA - Aeronautical & Astronautical Engineer\nEDU - Doctor of Education\nEE - Electrical Engineer\nEGD - Doctor of Engineering\nENE - Environmental Engineer\nENG - Engineer\nHM - Honorary Degree\nING - Engineering\nJD - Juris Doctor\nLLB - Bachelor of Laws\nLLD - Doctor of Laws\nLLM - Master of Laws\nMA - Master of Arts\nMAA - Master in Architecture Advanced Studies\nMAE - Materials Engineer\nMAR - Master of Architecture\nMAT - Master of Arts Teaching\nMBA - Masters in Business Administration\nMCP - Master in City Planning\nMD - Doctor of Medicine\nMDV - Master of Divinity\nME - Mechanical Engineer\nMEC - Master of Economics\nMED - Masters in Education\nMEE - Master of Mechnaical Engineering\nMEN - Master of Electrical Engineering\nMET - Meteorologist\nMFA - Master of Fine Arts\nMIE - Mineral Engineer\nMME - Marine Mechanical Engineer\nMMG - Master in Management\nMNG - Master of Engineering\nMO - Non Degree Master\nMPA - Master in Public Health Administration\nMPH - Master in Public Health\nMPL - Master in Patent Law\nMPP - Master of Public Policy\nMRE - Master of Research Development\nMS - Master of Science\nMSW - Master of Social Work\nMTE - Metallurgical Engineer\nMUD - Master of Urban Design\nMUP - Master of Urban Planning\nNA - Naval Architect\nNE - Naval Engineer\nNON - non degree\nNUE - Nuclear Engineer\nOCE - Ocean Engineer\nOD - Doctor of Optometry\nPHB - Bachelor of Philosophy\nPHC - Pharmaceutical Chemist\nPhD - Doctor of Philosophy\nPHM - Master of Philosophy\nPHS - Public Health Service\nSB - Bachelor of Science\nScD - Doctor of Science\nSE - Sanitary Engineer\nSM - Master of Science\n"
+          degrees.split("\n").map{|d| d.split('-').map {|i| i.strip}}.to_h
+        end
+      end
+
+      # MIT degree word sets are a hash with the degree abbreviation keys
+      # and the values are significant words that identify the degree (lowercase)
+      # The data structure can be used in analysis to disambiguate a degree label.
+      # @return degree_wordsets [Hash<String:Set<String>>]
+      def mit_degree_wordsets
+        @mit_degree_wordsets ||= mit_degrees.map {|k,v| [k, wordset(v.split)]}.to_h
+      end
+
+      # Convert an array of words into a set of significant words
+      # (minus nonwords).  Each word is converted to lower case.
+      # @param words [Array<String>] an array of words
+      # @return words [Set<String>] a set of lower case words - nonwords
+      def wordset(words)
+        words.map{|w| w.downcase.to_sym}.to_set - nonwords
+      end
+
+      # Words to be excluded from disambiguation analysis, like:
+      # of, in, the, and
+      # @return nonwords [Set<String>]
+      def nonwords
+        @nonwords ||= %w(& of in the and).map{|w| w.to_sym}.to_set
       end
 
     end
