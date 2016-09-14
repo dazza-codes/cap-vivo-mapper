3a3a3671f2d28dca3f67a84c69284b7b95d01da2
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-09-14 15:29:47 -0700

    Revise sample profile data (some students are no longer active in CAP)

--- bin/cap_sample_profiles.rb
+++ bin/cap_sample_profiles.rb
@@ -7,7 +7,7 @@ STAFF = [25845]
 #  4164 - Mark Musen (has postdoctoralAdvisees)
 #  4706 - Russ Altman  (has postdoctoralAdvisees)
 #  6367 - Craig Levin (has GraduateAndFellowshipPrograms and postdoctoralAdvisees)
-#       - has postdocs: 51528, 49024, 65880, 37136
+#       - has postdocs: 51528, 49024, 37136
 # 49224 - Michel Dumontier (has GraduateAndFellowshipPrograms and postdoctoralAdvisees)
 #       - has postdocs: 68722
 FACULTY = [4164, 4706, 6367, 48613, 49224]
@@ -19,30 +19,28 @@ PHYSICIANS = [5949,48613]
 # Students with advisors:
 # most postdocs have:
 # profile["stanfordAdvisors"][x]["position"] : "Postdoctoral Faculty Sponsor"
-# 47441,62351,37521,63779,53591,38061 # (same advisor)
-# 40494,39312,37987 # (different advisors) have both:
+# 47441,62351,37521,53591,38061 # (same advisor)
+# # (different advisors) have both:
 # profile["stanfordAdvisors"][x]["position"] : "Postdoctoral Faculty Sponsor"
 # profile["stanfordAdvisors"][x]["position"] : "Postdoctoral Research Mentor"
 # 21125
 # profile["stanfordAdvisors"][x]["position"] : "Postdoctoral Research Mentor"
-# 46806,61829,44963,45434,60202
+# 46806,61829,44963,60202
 # profile["stanfordAdvisors"][x]["position"] : "Doctoral (Program)"
 # 40059
 # profile["stanfordAdvisors"][x]["position"] : "Doctoral Dissertation Advisor (AC)"
-# 17761 - Ronald Alfa (SoM MD Student) also a PhD Student and MSTP Student
 # 34085 - Biafra Ahanonu (PhD Student)
 # 68722 - Amrapali Zaveri (SoM Postdoc) of Michel Dumontier (49224)
 STUDENTS = [
-  51528, 49024, 65880, 37136,
-  17761,34085,68722,
-  47441,62351,37521,63779,53591,38061,
-  40494,39312,37987,
+  51528, 49024, 37136,
+  34085,68722,
+  47441,62351,37521,53591,38061,
   21125,
-  46806,61829,44963,45434,60202,
+  46806,61829,44963,60202,
   40059]
 
 def test_profiles
   test_ids = STAFF.concat FACULTY.concat PHYSICIANS.concat STUDENTS
-  test_ids.uniq.shuffle!
+  #test_ids.uniq.shuffle!
 end
 
